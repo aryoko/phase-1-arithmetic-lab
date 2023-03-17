062a982a9016b73554b17af1dc2821a7f4387d07
Ary Okoli <aco.umeadi@gmail.com>
2023-03-17 19:10:33 -0400

    arithmetric lab complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 6.2;
+let num2 = 10;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Number(Math.ceil(Math.random()) * 10);
+
+const num3 = 36;
+const num4 = 8;
+let mod = num3 % num4;
+
+let max = Math.max(1, 4, 16, 20);
